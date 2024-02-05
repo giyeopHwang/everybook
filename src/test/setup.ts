25aab0a90d22f0c3fa80b7deaa1c385e6fff3fcf
Giyeop Hwang <lay_ers@Giyeops-MacBook-Air.local>
2024-02-05 17:31:12 +0900

    fix(canvas): supports canvas mock for vitest

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,6 +1,15 @@
 import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
+import 'vitest-canvas-mock';
 import '@testing-library/jest-dom';
+
+const ResizeObserverMock = vi.fn(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}));
+
+vi.stubGlobal('ResizeObserver', ResizeObserverMock);
 
 afterEach(() => {
   cleanup();
